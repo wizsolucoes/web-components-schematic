178c0346c2691fd79b4dd9d0d2fbaa934e3204bf
raulfernandez <raulmelo.web@gmail.com>
2023-03-17 01:29:58 -0300

    chore: create schematics

--- src/starter/files/src/app/shared/models/simulation-request.ts
+++
@@ -1,7 +0,0 @@
-export interface SimulationRequest {
-  name: string | undefined;
-  cpf: string | undefined;
-  email: string | undefined;
-  loanAmount: number | undefined;
-  quantity: number | undefined;
-}
